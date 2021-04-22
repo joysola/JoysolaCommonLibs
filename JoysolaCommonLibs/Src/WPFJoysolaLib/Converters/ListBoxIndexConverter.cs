55b601557da7d7a34f2514d6a07a920496d0eb4c
joysola <joysola315718801@gmail.com>
2021-04-22 10:26:22 +0800

    1. 上传初版工具类。

---
+++ JoysolaCommonLibs/Src/WPFJoysolaLib/Converters/ListBoxIndexConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPFJoysolaLib.Converters
+{
+    public class ListBoxIndexConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is ListBox listbox && values[1] is ListBoxItem item)
+            {
+                return (listbox.Items.IndexOf(item.DataContext) + 1).ToString();
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
