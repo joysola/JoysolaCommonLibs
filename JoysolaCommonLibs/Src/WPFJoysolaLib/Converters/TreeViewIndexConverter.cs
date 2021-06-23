ce363c05d3b7e5ffb7aa482e4b5588a497cc1e49
joysola <joysola315718801@gmail.com>
2021-06-23 09:13:19 +0800

    1. 更新树形序号菜单

--- JoysolaCommonLibs/Src/WPFJoysolaLib/Converters/TreeViewIndexConverter.cs
+++ JoysolaCommonLibs/Src/WPFJoysolaLib/Converters/TreeViewIndexConverter.cs
@@ -5,19 +5,21 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace WPFJoysolaLib.Converters
 {
     /// <summary>
     /// 树形控件index
     /// </summary>
-    public class TreeViewIndexConverter : IMultiValueConverter, IValueConverter
+    public class TreeViewIndexConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values != null && values.Length == 2 && values[0] is TreeView tree && values[1] is TreeViewItem item)
             {
-                var result = GetIndexfromTree(tree, item, 0);
+                var list = FindTreeViewItems(tree);
+                var result = list.IndexOf(item) + 1;
                 return $"{result}.";
             }
             return DependencyProperty.UnsetValue;
@@ -27,70 +29,45 @@ namespace WPFJoysolaLib.Converters
         {
             throw new NotImplementedException();
         }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-
         /// <summary>
-        /// 从树中找对应treeitem的index
+        /// 获取所有treeitem
         /// </summary>
-        /// <param name="tree">树</param>
-        /// <param name="treeItem">要找的item</param>
-        /// <param name="index">初始索引</param>
+        /// <param name="this"></param>
         /// <returns></returns>
-        private int GetIndexfromTree(TreeView tree, TreeViewItem treeItem, int index)
+        private List<TreeViewItem> FindTreeViewItems(Visual @this)
         {
-            foreach (var item in tree.Items)
+            if (@this == null)
+                return null;
+
+            var result = new List<TreeViewItem>();
+
+            var frameworkElement = @this as FrameworkElement;
+            if (frameworkElement != null)
             {
-                index++;
-                var curTreeItem = tree.ItemContainerGenerator.ContainerFromItem(item) as TreeViewItem;
-                if (curTreeItem == item)
+                frameworkElement.ApplyTemplate();
+            }
+
+            Visual child = null;
+            for (int i = 0, count = VisualTreeHelper.GetChildrenCount(@this); i < count; i++)
+            {
+                child = VisualTreeHelper.GetChild(@this, i) as Visual;
+
+                var treeViewItem = child as TreeViewItem;
+                if (treeViewItem != null)
                 {
-                    break;
+                    result.Add(treeViewItem);
+                    if (!treeViewItem.IsExpanded)
+                    {
+                        treeViewItem.IsExpanded = true;
+                        treeViewItem.UpdateLayout();
+                    }
                 }
-                var success = GetIndex(curTreeItem, treeItem, ref index);
-                if (success) // 成功找到
+                foreach (var childTreeViewItem in FindTreeViewItems(child))
                 {
-                    break;
+                    result.Add(childTreeViewItem);
                 }
             }
-            return index;
-        }
-        /// <summary>
-        /// 寻找当前curTreeItem里是否包含treeItem
-        /// </summary>
-        /// <param name="curTreeItem">当前item</param>
-        /// <param name="treeItem">需要寻找的item</param>
-        /// <param name="index">索引</param>
-        /// <returns></returns>
-        private bool GetIndex(TreeViewItem curTreeItem, TreeViewItem treeItem, ref int index)
-        {
-            if (curTreeItem == treeItem) // 直接找到
-            {
-                return true;
-            }
-            foreach (var item in curTreeItem.Items)
-            {
-                index++;
-                var targetItem = curTreeItem.ItemContainerGenerator.ContainerFromItem(item) as TreeViewItem;
-                if (targetItem != null && treeItem == targetItem)
-                {
-                    return true;
-                }
-                else // 未找到
-                {
-                    GetIndex(targetItem, treeItem, ref index);
-                }
-            }
-            return false;
+            return result;
         }
     }
 }
